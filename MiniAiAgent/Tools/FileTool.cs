26edb794746b10339b12b4a37ddb7c9dd3598acb
MehmetCekicPXL <mehmet.cekic@student.pxl.be>
2026-08-14 14:01:55 +0200

    ListFiles en ReadFile methodes toegevoegs

---
+++ MiniAiAgent/Tools/FileTool.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniAiAgent.Tools
+{
+    public class FileTool
+    {
+        public string[] ListFiles(string directory)
+        {
+            return Directory.GetFiles(
+                directory,"*",SearchOption.AllDirectories);
+        }
+
+        public string ReadFile(string filePath)
+        {
+            return File.ReadAllText(filePath);
+        }
+    }
+}
