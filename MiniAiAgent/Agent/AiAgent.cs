28bfe6be86e6e42b3e49578ed849ea0434ced2f9
MehmetCekicPXL <mehmet.cekic@student.pxl.be>
2026-08-14 14:13:26 +0200

    eerst agent loop logica

--- MiniAiAgent/Agent/AiAgent.cs
+++ MiniAiAgent/Agent/AiAgent.cs
@@ -10,8 +10,24 @@ namespace MiniAiAgent.Agent
     {
         public void Run()
         {
-            Console.WriteLine("AI Agent is running...");
-            // Add your AI agent logic here
+           // Console.WriteLine("AI Agent is running...");
+           // Add your AI agent logic here
+           while(true)
+            {
+                Console.Write("wat will je doen?  -->   ");
+                var input= Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(input))
+                {
+                    continue;
+                }
+                if(input.Equals("exit", StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine("Exiting AI Agent...");
+                    break;
+                }
+                Console.WriteLine($"You entered: {input}");
+            }
+
         }
     }
 }
