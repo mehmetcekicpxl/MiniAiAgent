fe639227d57e62eb686fa71c8b71b8d975536b28
MehmetCekicPXL <mehmet.cekic@student.pxl.be>
2026-08-14 14:56:35 +0200

    tool calling functie

---
+++ MiniAiAgent/Models/ToolCall.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiniAiAgent.Models
+{
+    public class ToolCall
+    {
+        public string ToolName { get; set; } = string.Empty;
+        public Dictionary<string, string> Arguments { get; set; } = new();
+
+    }
+}
