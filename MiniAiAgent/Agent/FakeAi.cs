9c91528e7c2b9f0c81b4dd4526f98b4c8a951232
MehmetCekicPXL <mehmet.cekic@student.pxl.be>
2026-08-14 15:00:44 +0200

    fake ai voor testen

---
+++ MiniAiAgent/Agent/FakeAi.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MiniAiAgent.Models;
+
+namespace MiniAiAgent.Agent
+{
+    public class FakeAi
+    {
+        public ToolCall? Decide( string userInput)
+        {
+            if (userInput.Contains("list files", StringComparison.OrdinalIgnoreCase))
+            {
+                return new ToolCall
+                {
+                    ToolName = "ListFiles",
+                    
+                };
+            }
+            return null;
+               
+        }
+    }
+}
